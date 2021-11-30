f4f2595840fe6661b9a98944d446adcfc3b32157
KlaasHagge <klaash@ppedv.de>
2021-11-30 10:51:23 +0100

    Projektdateien hinzufügen.

---
+++ HelloWorld/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            System.Console.WriteLine("Hello, World!");
+
+            int Alter;
+            Alter = 32;
+            Console.WriteLine(Alter);
+
+            string Stadt = "Berlin";
+            Console.WriteLine(Stadt);
+
+            int DoppltesAlter = Alter * 2;
+            Console.WriteLine(DoppltesAlter);
+        }
+    }
+}
+
