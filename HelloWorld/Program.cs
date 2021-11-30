0d2df0bc1e55a2142d93b62e9396337000afb835
KlaasHagge <klaash@ppedv.de>
2021-11-30 12:18:04 +0100

    Grundlagen (Variablen, Datentypen, Strings, Casts)

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -1,23 +1,106 @@
-﻿using System;
+﻿//Mittels der USING-Anweisungen kann ein vereinfachter Zugriff auf Programm-Externe Klassen ermöglicht werden. Es muss nun nicht mehr der
+///vollständige Pfad angegeben werden, sondern es reicht der Klassenbezeichner
+using System;
 
+//NAMESPACE: Die Umgebung unseres aktuellen Programms: Alles innerhalb des Namespaces gehört zu dem Programm
 namespace HelloWorld
 {
-    public class Program
+    //Die PROGRAM-Klasse beinhaltet den Einstiegspunkt des Programms und muss in jedem C#-Programm vorhanden sein
+    class Program
     {
+        //Die MAIN()-Methode ist der Einstiegspunkt jedes C#-Programms: Hier beginnt das Programm IMMER
         static void Main(string[] args)
         {
-            System.Console.WriteLine("Hello, World!");
+            //Ausgabe eines String-Literals (Literale sind 'ausgeschriebene' Werte, im Gegensatz zu Werten die in einer Variablen stecken)
+            Console.WriteLine("Hello World!");
 
+            //Deklaration einer Integer-Variable 
             int Alter;
+            //Initialisierung der Integer-Variablen
             Alter = 32;
-            Console.WriteLine(Alter);
-
+            //Gleichzeitige Deklaration und Initialisierung einer String-Variablen
             string Stadt = "Berlin";
+            //Ausgabe der Variablen
+            Console.WriteLine(Alter);
             Console.WriteLine(Stadt);
+            //Deklaration und Initialisierung einer Integer-Variablen mithilfe einer anderen Integer-Variablen
+            int DoppeltesAlter = Alter * 2;
+            Console.WriteLine(DoppeltesAlter);
+            //Int-Berechnung innerhalb String-Ausgabe
+            Console.WriteLine(Alter + DoppeltesAlter);
 
-            int DoppltesAlter = Alter * 2;
-            Console.WriteLine(DoppltesAlter);
+            //Beispiel-Char-Variable
+            char Textzeichen = 'A';
+            //Beispiel-Double-Variable
+            double Kosten = 78.123;
+            Console.WriteLine(Kosten);
+
+
+            ///Einfügen dynamischer Inhalte in Strings
+            //'traditionell' über Stringverknüpfung (+-Operator)
+            string Satz = "Ich bin " + Alter + " Jahre alt und wohne in " + Stadt + ".";
+            Console.WriteLine(Satz);
+            Console.WriteLine("Ich bin " + Alter + " Jahre alt und wohne in " + Stadt + ".");
+            //$-Operator (Variablen werde direkt in {}-Klammern geschrieben)
+            Satz = $"Ich bin {Alter} Jahre alt und wohne in {Stadt}.";
+            Console.WriteLine(Satz);
+            Console.WriteLine($"Ich bin {Alter} Jahre alt und wohne in {Stadt}.");
+            //Index (Variablen werden durch Index-Platzhalter vertreten und später definiert)
+            Console.WriteLine("Ich bin {0} Jahre alt und wohne in {1}.", Alter, Stadt);
+
+            //Ausgabe einer Berchnung in Strings
+            int a = 45;
+            int b = 12;
+            Console.WriteLine($"{a} + {b} = {a + b}");
+
+            //String-Formatierung mittels Escape-Sequenzen
+            string bsp = "Dies ist ein \tTabulator und dies ein \nZeilenumbruch.";
+            Console.WriteLine(bsp);
+            //Bsp für Pfadausgabe mittels Escape-Sequenzen
+            string path = "C:\\Programme\\Programm.exe";
+            Console.WriteLine(path);
+
+            //String-Formatierung mittels VerbaTim-String (Einleitung mittels @ / Escape-Sequenzen sind nicht möglich, dynamische Inhalte mittels $ schon)
+            string verbatim = @$"Dies ist ein    Tabulator und dies ein 
+Zeilenumbruch {Stadt}";
+            Console.WriteLine(verbatim);
+            //Bsp für Pfadausgabe in Verbatim-String
+            path = @"C:\Programme\Programm.exe";
+
+
+            //Eingabe eines Strings durch den Benutzer und Abspeichern in einer String-Variablen
+            Console.WriteLine("Bitte gib deinen Namen an:");
+            string eingabe = Console.ReadLine();
+            //Ausgabe
+            Console.WriteLine($"Dein Name ist also {eingabe}.");
+
+
+            //Eingabe eines Strings, Umwandlung in einen Integer (Parse()-Funktion) und Abspeichern in einer Integer-Variablen
+            Console.WriteLine("Bitte gib deine Lieblingszahl ein:");
+            string zahlAlsString = Console.ReadLine();
+            int zahl = int.Parse(zahlAlsString);
+            zahl = zahl * 2;
+            //Ausgabe
+            Console.WriteLine(zahl);
+
+            //Benutzereingabe (Tastendruck) hier als Programmpause, bis Benutzer einer Taste drückt
+            Console.ReadKey();
+
+            //Umwandlung durch Convert.To[]()-Funktion
+            zahl = Convert.ToInt32(78.45);
+
+            //Bsp für numerische Umwandlung (impliziet, da kein Datenverlust)
+            int intZahl = 78;
+            double doubleZahl = intZahl;
+
+            //Bsp für numerische Umwandlung mittels Cast (expliziet, da möglicherweise Datenverlust)
+            doubleZahl = 45.75;
+            intZahl = (int)doubleZahl;
+
+            //Bsp für Teilung durch 0 von Gleitkommazahlen
+            double zero = 0.0;
+            double z = 2 / zero;
+            Console.WriteLine(z);
         }
     }
 }
-
---
+++ README.md
@@ -0,0 +1,140 @@
+﻿# CSharp Grundkurs
+
+Trainer: Klaas Hagge
+Darum: 30.11.2021
+
+## M00 | VORSTELLUNG
+
+- [ ] Begrüßung
+- [ ] Ablauf
+- [ ] Agenda
+
+
+## M01 | .NET GRUNDLAGEN
+- [ ] C#
+- [ ] .NET 5.0
+- [ ] Projekte und Projektmappen
+- [ ] HelloWorld.cs
+- [ ] Startprojekt festlegen
+
+
+## M02 | VARIABLEN, DATENTYPEN, KONVERTIERUNGEN und OPERATOREN
+- [ ] Sprachgundlagen 
+- [ ] Variablen & Datentypen 
+- [ ] Aus- und Eingaben
+- [ ] Parsing & Casting
+- [ ] Operatoren  
+ 
+
+## M03 | ARRAYS und BEDINGUNGEN
+- [ ] eindimensionale Arrays 
+- [ ] mehdimensionale Arrays 
+- [ ] logische Operatoren
+- [ ] if-Abfrage 
+
+## M04 | SCHLEIFEN und ENUMS
+- [ ] while-Schleifen
+- [ ] do-while-Schleife
+- [ ] for-Schleife  
+- [ ] foreach-Schleife  
+- [ ] Enums 
+
+
+## M05 | Funktionen
+- [ ] Funktionen  
+- [ ] Modifier  
+- [ ] Parameter 
+- [ ] optionale Parameter 
+- [ ] params   
+- [ ] out  
+- [ ] Überladung 
+
+
+## M06 | OOP - NAMESPACES, KLASSEN, FELDER und KAPSELUNGEN
+- [ ] Namespaces 
+- [ ] Klassen  
+- [ ] Felder
+- [ ] Eigenschaften  
+- [ ] Kapselungen 
+- [ ] Konstruktoren 
+- [ ] Objekte 
+
+
+## M07 | OOP - GARBAGE COLLECTION, STATISCHE MEMBER, WERTE und REFERENZEN
+- [ ] Destruktor 
+- [ ] Garbage Collection
+- [ ] Statische Member
+- [ ] Werte- und Referenztypen
+- [ ] ref 
+
+
+## M08 | OOP - VERERBUNG und ZUGRIFFSMODIFIER
+- [ ] Vererbung
+- [ ] base
+- [ ] virtuelle Member   
+- [ ] Überschreibungen verhindern
+- [ ] Zugriffsmodifier 
+
+
+## M09 | OOP - POLYMORPHISMUS und TYPEN
+- [ ] Polymorphismus
+- [ ] Typtest "GetType()" & "typeof"  
+- [ ] Typtest "is" 
+- [ ] Member ausblenden 
+- [ ] abstrakte Member und Klassen 
+
+
+## M10 | OOP -INTERFACES
+- [ ] Interfaces  
+- [ ] Interface-Implementierungen  
+
+
+## M11 | GENERISCHE LISTEN
+- [ ] Generische Datentypen
+- [ ] List<T>
+- [ ] Stack<T>
+- [ ] Queue<T>
+- [ ] Dictionary<T1, T2>
+
+
+## M12 | FEHLERBEHANDLUNG und UNIT-TESTS
+- [ ] try-catch  
+- [ ] eigene Exceptions 
+- [ ] UnitTests
+
+
+## M13 | DELEGATES und EVENTS
+- [ ] Action 
+- [ ] Predicate   
+- [ ] Func
+- [ ] anonyme Methoden
+- [ ] EventHandler
+
+
+## M14 | GUI und WINDOWS FORMS
+- [ ] Windows Forms
+- [ ] partielle Klassen
+- [ ] WinForms in VS
+- [ ] Eigenschaften und Events
+- [ ] wichtige Controls
+- [ ] MessageBox
+- [ ] Schließen und Öffnen von Forms
+
+
+## M15 | DATEIEN und VERZEICHNISSE
+- [ ] Dateien lesen/schreiben 
+- [ ] Dateidialog
+- [ ] JSON
+- [ ] XML
+
+
+## M16 | WEITERE THEMEN
+- [ ] Operatoren überladen
+- [ ] IEnumerable
+- [ ] Indexer
+- [ ] Erweiterungsmethoden
+- [ ] LINQ
+- [ ] Timer
+
+-Alternativ können hier je nach Zeit und Teilnehmerbedarf auch andere Themen (z.B. Deployment / Klassendiagramme) durchgenommen
+	oder evtl ein Abschlussprojekt durchgeführt werden (vgl. hierfür M016-(Alternativen)).
