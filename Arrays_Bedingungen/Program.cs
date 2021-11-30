4ae5e2719c3ef0a84da3ecec376381e02baa9dd4
KlaasHagge <klaash@ppedv.de>
2021-11-30 15:19:45 +0100

    Modul 03: Arrays, Bedingungen

---
+++ Arrays_Bedingungen/Program.cs
@@ -0,0 +1,45 @@
+﻿
+
+//int[] zahlen = { 2, 4, 5, 79, -789, -8, 0, 11111 };
+
+//Console.WriteLine(zahlen[2]);
+
+//zahlen[2] = 1234;
+//Console.WriteLine(zahlen[2]);
+
+//Console.WriteLine(zahlen.Count());
+
+//Console.WriteLine(zahlen.Contains(78));
+
+
+//string[] Wörter = new string[10];
+
+
+//string beispiel = "Hallo";
+//Console.WriteLine(beispiel[1]);
+
+
+
+
+int a = 18;
+int b = 16;
+
+if(a < b)
+{
+    Console.WriteLine("A ist kleiner als B");
+}
+else if(a > b && a == 17)
+{
+    Console.WriteLine("B ist kleiner als A und A ist gleich 17");
+}
+else if(a > b)
+{
+    Console.WriteLine("B ist kleiner als A");
+}
+else
+{
+
+    Console.WriteLine("A ist gleich B");
+}
+
+Console.WriteLine("Ende des IF-Blocks");
