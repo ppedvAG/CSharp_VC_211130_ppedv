93998dbd4d96765af859dea5c28a226771440eee
KlaasHagge <klaash@ppedv.de>
2021-11-30 15:37:14 +0100

    Modul 02 / Lab 02 (kommentiert)

--- Arrays_Bedingungen/Program.cs
+++ Arrays_Bedingungen/Program.cs
@@ -1,45 +1,92 @@
-﻿
+﻿using System;
+using System.Linq;
 
-//int[] zahlen = { 2, 4, 5, 79, -789, -8, 0, 11111 };
-
-//Console.WriteLine(zahlen[2]);
-
-//zahlen[2] = 1234;
-//Console.WriteLine(zahlen[2]);
-
-//Console.WriteLine(zahlen.Count());
-
-//Console.WriteLine(zahlen.Contains(78));
-
-
-//string[] Wörter = new string[10];
-
-
-//string beispiel = "Hallo";
-//Console.WriteLine(beispiel[1]);
-
-
-
-
-int a = 18;
-int b = 16;
-
-if(a < b)
+namespace Array_Bedingungen
 {
-    Console.WriteLine("A ist kleiner als B");
-}
-else if(a > b && a == 17)
-{
-    Console.WriteLine("B ist kleiner als A und A ist gleich 17");
-}
-else if(a > b)
-{
-    Console.WriteLine("B ist kleiner als A");
-}
-else
-{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Arrays
 
-    Console.WriteLine("A ist gleich B");
-}
+            //ARRAYS
+            ///Arrays sind Collections, welche mehrere Variablen eines Datentyps speichern können. Die Größe des Arrays muss bei der
+            ///Initialisierung entweder durch eine Zahl oder durch eine bestimmte Anzahl von spezifischen Elementen definiert werden.
+            int[] zahlen = { 2, 4, 78, -123, -8, 0, 11111 };
+            //Der Zugiff auf einzelne Array-Positionen erfolgt durch einen Nullbasierten Index
+            Console.WriteLine(zahlen[2]);
+            zahlen[2] = 1234;
+            Console.WriteLine(zahlen[2]);
 
-Console.WriteLine("Ende des IF-Blocks");
+            //Array-Deklaration ohne direkte Initialisierung der Positionen (Größe muss angegeben werden)
+            string[] worte = new string[5];
+
+            //Verwendung der Contains-Funktion eines Arrays (überprüft auf das Vorhandensein eines Elements)
+            Console.WriteLine(zahlen.Contains(-123));
+            Console.WriteLine(zahlen.Contains(555));
+
+            //Ausgabe der Länge (Anzahl der Elemente) des Arrays
+            Console.WriteLine(zahlen.Length);
+
+            //Stringarray mit 10 leeren Plätzen
+            string[] Wörter = new string[10];
+
+            //String als Char-Array
+            string beispiel = "Hallo";
+            Console.WriteLine(beispiel[2]);
+
+            //Mehrdimensionales Array
+            int[,] zweiDimArray = new int[3, 5];
+            zweiDimArray[0, 0] = 1;
+
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 5; j++)
+                {
+                    zweiDimArray[i, j] = i * j;
+                }
+            }
+            Console.WriteLine(zweiDimArray[2, 3]);
+            #endregion
+
+
+
+            #region Bedingungen (If/Else)
+
+            //Deklaration und Initialisierung von Beispiel-Variablen
+            int a = 23;
+            int b = 23;
+
+            //IF-ELSEIF-ELSE-Block
+            ///Das Programm wird den ersten Block ausführen, bei welchem er auf eine wahre Bedingung trifft und dann am Ende des Blocks mit
+            ///dem Code weiter machen
+            if (a < b)
+            {
+                Console.WriteLine("A ist kleiner als B");
+            }
+            //Es kann beliebig viele ELSE-IF-Blöcke geben
+            else if (a > b)
+            {
+                Console.WriteLine("A ist größer als B");
+            }
+            //Wenn keine der Bedingungen wahr ist, wird der (optionale) ELSE-Block ausgeführt
+            else
+                Console.WriteLine("A ist gleich B");
+
+            //Kurznotation:
+            //(Bedingung) ? TrueAusgabe : FalseAusgabe
+            string ergebnis = (a == b) ? "A ist gleich B" : "A ist ungleich B";
+
+            //Bsp eines String-Vergleichs
+            string name1 = "Hans";
+            string name2 = "Hans";
+
+            if (name1.Equals(name2))
+            {
+                Console.WriteLine("Gleich");
+            }
+            #endregion
+
+        }
+    }
+}
---
+++ Lab03_Schaltjahrrechner_MiniLotto/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Linq;
+
+namespace Schaltjahrrechner_MiniLotto
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            #region 1.Aufgabe: Schaltjahr-Rechner
+
+            //Abfrage der Eingabe
+            Console.WriteLine("Gib das Jahr ein:");
+            int eingabe = int.Parse(Console.ReadLine());
+
+            //Deklarierung/Initialisierung der bool-Variablen
+            bool istSchaltjahr = false;
+
+            //Prüfung einer Teilbarkeit durch 4
+            if (eingabe % 4 == 0)
+            {
+                //Setzten der Variablen auf true
+                istSchaltjahr = true;
+
+                //Prüfung einer Teilbarkeit durch 100
+                if (eingabe % 100 == 0)
+                {
+                    //Setzten der Variablen auf false
+                    istSchaltjahr = false;
+
+                    //Prüfung einer Teilbarkeit durch 400
+                    if (eingabe % 400 == 0)
+                        //Setzten der Variablen auf true
+                        istSchaltjahr = true;
+                }
+            }
+
+            //Ausgabe
+            Console.WriteLine($"{eingabe} ist Schaltjahr: {istSchaltjahr}");
+
+            //Alternative (detailiertere) Ausgabe mittels Kurz-Bedingung
+            string ausgabe = istSchaltjahr ? $"{eingabe} ist ein Schaltjahr." : $"{eingabe} ist kein Schaltjahr.";
+            Console.WriteLine(ausgabe + "\n\n\n");
+
+            #endregion
+
+
+            #region 2. Aufgabe: Mini-Lotto
+
+            //Deklaration & Initialisierung des Arrays der Gewinnzahlen
+            int[] gewinnzahlen = { 3, 16, 45, 79, 99 };
+
+            //Abfrage des User-Tipps
+            Console.Write("Bitte gib deinen Tipp ab (Ganzzahl zwischen 0 und 100): ");
+            int tipp = int.Parse(Console.ReadLine());
+
+            //Prüfung des Zahlenbereiches des Tipps
+            if (tipp < 0 || tipp > 100)
+                Console.WriteLine("Dein Tipp ist außerhalb des Zahlenbereiches.");
+            else
+            {
+                //Prüfung, ob Tipp eine Gewinnzahl ist und Ausgabe
+                if (gewinnzahlen.Contains(tipp))
+                    Console.WriteLine("Glückwunsch!! Du hast eine der fünf Gewinnzahlen getippt.");
+                else
+                    Console.WriteLine("Leider daneben. Viel Glück beim nächsten Mal.");
+            } 
+            #endregion
+        }
+    }
+}
